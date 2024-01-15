8318aedb4b89260f189dffd4ae2c8e2bdef8cea1
RCV29 <roco.visperas.up@phinmaed.com>
2024-01-15 20:40:39 +0800

    first commit

---
+++ crud-app/resources/views/students/edit.blade.php
@@ -0,0 +1,28 @@
+@extends('students.layout')
+@section('content')
+ 
+<div class="card">
+  <div class="card-header">Contactus Page</div>
+  <div class="card-body">
+      
+      <form action="{{ url('student/' .$students->id) }}" method="post">
+        {!! csrf_field() !!}
+        @method("PATCH")
+        <input type="hidden" name="id" id="id" value="{{$students->id}}" id="id" />
+        <label>Name</label></br>
+        <input type="text" name="name" id="name" value="{{$students->name}}" class="form-control"></br>
+        <label>Age</label></br>
+        <input type="text" name="age" id="age" value="{{$students->age}}" class="form-control"></br>
+        <label>Address</label></br>
+        <input type="text" name="address" id="address" value="{{$students->address}}" class="form-control"></br>
+        <label>Course</label></br>
+        <input type="text" name="course" id="course" value="{{$students->course}}" class="form-control"></br>
+        <label>Subject</label></br>
+        <input type="text" name="subject" id="subject" value="{{$students->subject}}" class="form-control"></br>
+        <input type="submit" value="Update" class="btn btn-success"></br>
+    </form>
+   
+  </div>
+</div>
+ 
+@stop
---
+++ crud-app/resources/views/students/show.blade.php
@@ -0,0 +1,21 @@
+@extends('students.layout')
+@section('content')
+ 
+ 
+<div class="card">
+  <div class="card-header">Students Page</div>
+  <div class="card-body">
+   
+ 
+        <div class="card-body">
+        <h5 class="card-title">Name : {{ $students->name }}</h5>
+        <p class="card-text">Age : {{ $students->age }}</p>
+        <p class="card-text">Address : {{ $students->address }}</p>
+        <p class="card-text">Course : {{ $students->course }}</p>
+        <p class="card-text">Subject : {{ $students->subject }}</p>
+  </div>
+       
+    </hr>
+  
+  </div>
+</div>
